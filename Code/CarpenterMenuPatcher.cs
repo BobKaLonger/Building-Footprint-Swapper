cc846c864cdc48ca53ff7b849268cc00d54e0f40
BobKaLonger <rkollingeriii@gmail.com>
2026-03-28 15:21:58 -0400

    deferred build cost, added postfix and let upgrade progress instead of insta-finish

--- Code/CarpenterMenuPatcher.cs
+++ Code/CarpenterMenuPatcher.cs
@@ -7,6 +7,7 @@ using StardewValley.GameData.Buildings;
 using StardewValley.Menus;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace BuildingFootprintSwapper;
 
@@ -26,9 +27,14 @@ internal static class CarpenterMenuPatcher
     {
         harmony.Patch(
             original: AccessTools.Method(typeof(CarpenterMenu), nameof(CarpenterMenu.receiveLeftClick)),
-            prefix: new HarmonyMethod(typeof(CarpenterMenuPatcher), nameof(receiveLeftClick_Prefix))
+            prefix: new HarmonyMethod(typeof(CarpenterMenuPatcher), nameof(receiveLeftClick_Prefix)),
+            postfix: new HarmonyMethod(typeof(CarpenterMenuPatcher), nameof(receiveLeftClick_Postfix))
         );
     }
+
+    private static PendingUpgradeCost _pendingCost;
+
+    private record PendingUpgradeCost(int Gold, List<Item> Materials);
 
     [HarmonyPrefix]
     private static bool receiveLeftClick_Prefix(CarpenterMenu __instance, int x, int y)
@@ -49,16 +55,38 @@ internal static class CarpenterMenuPatcher
         if (toUpgrade == null || toUpgrade.buildingType.Value != __instance.Blueprint.UpgradeFrom)
             return true;
 
-        // --- Custom upgrade-then-move flow ---
+        _pendingCost = new PendingUpgradeCost(
+            __instance.Blueprint.BuildCost,
+            __instance.Blueprint.BuildMaterials
+                .Select(m => ItemRegistry.Create(m.ItemId, m.Amount))
+                .ToList()
+        );
 
-        // 1. Deduct resources exactly as the normal upgrade flow would
-        __instance.ConsumeResources();
-
-        // 2. Set up the upgrade and finish it instantly so the building reflects
-        //    the new type and footprint before we hand it to move mode
         toUpgrade.upgradeName.Value = __instance.Blueprint.Id;
-        toUpgrade.daysUntilUpgrade.Value = 0;
-        toUpgrade.FinishConstruction();
+        toUpgrade.daysUntilUpgrade.Value = Math.Max(__instance.Blueprint.BuildDays, 1);
+        toUpgrade.showUpgradeAnimation(__instance.TargetLocation);
+        Game1.netWorldState.Value.MarkUnderConstruction(__instance.Builder, toUpgrade);
+        Game1.Multiplayer.globalChatInfoMessage(
+            "BuildingBuild",
+            Game1.player.Name,
+            "aOrAn:" + __instance.Blueprint.TokenizedDisplayName,
+            __instance.Blueprint.TokenizedDisplayName,
+            Game1.player.farmName.Value
+        );
+
+        if (Game1.buildingData.TryGetValue(__instance.Blueprint.Id, out BuildingData newData))
+        {
+            toUpgrade.tilesWide.Value = newData.Size.X;
+            toUpgrade.tilesHigh.Value = newData.Size.Y;
+        }
+        else
+        {
+            ModEntry.ModMonitor.Log(
+                $"Could not find building data for '{__instance.Blueprint.Id}' to update footprint dimensions" +
+                $"Move mode may show incorrect placement overlay.",
+                LogLevel.Warn
+            );
+        }
 
         __instance.Action = CarpenterMenu.CarpentryAction.Move;
         toUpgrade.isMoving = true;
@@ -67,12 +95,37 @@ internal static class CarpenterMenuPatcher
         Game1.playSound("axchop");
 
         ModEntry.ModMonitor.Log(
-            $"Upgrade of '{toUpgrade.buildingType.Value}' finished instantly. " +
+            $"Upgrade of '{__instance.Blueprint.Id}' queued. " +
             $"Entering move mode for repositioning.",
             LogLevel.Debug
         );
 
         return false;
+    }
+
+    [HarmonyPostfix]
+    private static void receiveLeftClick_Postfix(CarpenterMenu __instance)
+    {
+        if (_pendingCost == null || __instance.buildingToMove != null)
+            return;
+
+        if (!__instance.onFarm || __instance.Action != CarpenterMenu.CarpentryAction.Move)
+        {
+            _pendingCost = null;
+            return;
+        }
+
+        Game1.player.Money -= _pendingCost.Gold;
+
+        foreach (Item material in _pendingCost.Materials)
+            Game1.player.Items.ReduceId(material.QualifiedItemId, material.Stack);
+
+        ModEntry.ModMonitor.Log(
+            $"Building placed successfully. Build costs deducted.",
+            LogLevel.Debug
+        );
+
+        _pendingCost = null;
     }
 
     private static bool IsForceMoveBlueprintUpgrade(CarpenterMenu.BlueprintEntry blueprint)
